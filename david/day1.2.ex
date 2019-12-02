6681da5e24f9ec1d5f9bb76db1423f7a7f193b9e
David Lewis <david.lewis@databricks.com>
2019-12-02 07:44:10 -0700

    day 1.2 for reals

---
+++ david/day1.2.ex
@@ -0,0 +1,9 @@
+lines = File.stream!("data/day1")
+
+total = lines
+  |> Stream.map(&String.trim/1)
+  |> Stream.filter(&("" != &1)) 
+  |> Stream.map(&String.to_integer/1)
+  |> Stream.map(&Day1.totalRequiredFuel/1)
+  |> Enum.sum
+IO.puts total
