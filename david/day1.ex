c9cf01ad10937be11206fe094e131dce95c20cf2
David Lewis <david.lewis@databricks.com>
2019-12-02 07:43:34 -0700

    day 1.2!

--- david/day1.ex
+++ david/day1.ex
@@ -1,5 +1,4 @@
-file = File.read!("data/day1")
-lines = String.split(file, "\n")
+lines = File.stream!("data/day1")
 
-total = lines |> Stream.filter(&("" != &1)) |> Stream.map(&String.to_integer/1) |> Stream.map(&Day1.requiredFuel/1) |> Enum.sum
+total = lines |> Stream.map(&String.trim/1) |> Stream.filter(&("" != &1)) |> Stream.map(&String.to_integer/1) |> Stream.map(&Day1.requiredFuel/1) |> Enum.sum
 IO.puts total
--- david/day1.exs
+++ david/day1.exs
@@ -2,4 +2,10 @@ defmodule Day1 do
   def requiredFuel(mass) do
     trunc(mass / 3) - 2
   end
+  def totalRequiredFuel(mass) do
+    Stream.iterate(mass, &requiredFuel/1)
+    |> Stream.drop(1)
+    |> Stream.take_while(&(&1 > 0))
+    |> Enum.sum
+  end
 end
