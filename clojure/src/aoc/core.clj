c4869f965d24d5f953b9d48cd78263d92e558c4c
Ryan Sargent <rwsargent@google.com>
2019-12-02 16:01:13 -0800

    rearranged day01 to be cooler :)

--- clojure/src/aoc/core.clj
+++ clojure/src/aoc/core.clj
@@ -1,16 +1,12 @@
 (ns aoc.core
   (:require
    [clojure.java.io :as io]
+   [aoc.day01 :as day01]
   (:gen-class)))
 
-
-(def input (map #(Integer/parseInt %) (->> "day01.txt" io/resource io/reader line-seq)))
-;;(println (reduce + (map #( - %1 2) (map #( quot %1 3) input))))
-
-(defn calc-fuel [mass]
-  (def req-fuel (- (quot mass 3) 2))
-  (if (< req-fuel 0)
-    0
-    (+ req-fuel (calc-fuel req-fuel))))
-
-(println (reduce + (map calc-fuel input)))
+(defn -main
+  [& args]
+  (println(case (first args)
+    "day01" [(day01/part-one) (day01/part-two)]
+    "default" "unknown arg"
+  )))
