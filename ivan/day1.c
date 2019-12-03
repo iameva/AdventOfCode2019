83d75f91df3971f35e6af566fd1dc637149aa35b
Ivan Miller <ivanwakeup@Ivans-MacBook-Pro.local>
2019-12-03 12:04:15 -0700

    adds day1 part 2 solution, remove binaries from source control

--- ivan/day1.c
+++ ivan/day1.c
@@ -6,32 +6,59 @@ int calc_fuel_rqrt(int mass) {
     return (mass/3)-2;
 }
 
-int calc_total_fuel() {
-    FILE *fptr;
-    char filename[14] = "data/day1.txt";
+int calc_total_fuel(FILE *fptr) {
     char buffer[100];
     int total = 0;
 
-    fptr = fopen(filename, "r");
     if(fptr == NULL) {
         printf("couldn't read data file \n");
         exit(0);
     }
-
     /*struggled with some of the input. reading the file via fgetc() is MUCH? more difficult than using fgets */
     while(fgets(buffer, 100, fptr) != NULL){
         int value = atoi(buffer);
         int fuel_rqrd = calc_fuel_rqrt(value);
         total+=fuel_rqrd;
     }
-    fclose(fptr);
     return total;
+}
 
+int calc_fuel_recursive(FILE *fptr) {
+    char buffer[100];
+    int total = 0;
+
+    if(fptr == NULL) {
+        printf("couldn't read data file \n");
+        exit(0);
+    }
+    /*struggled with some of the input. reading the file via fgetc() is MUCH? more difficult than using fgets */
+    while(fgets(buffer, 100, fptr) != NULL){
+        int value = atoi(buffer);
+        int local = 0;
+        int fuel_rqrd = calc_fuel_rqrt(value);
+        while(fuel_rqrd > 0) {
+            local+=fuel_rqrd;
+            fuel_rqrd=calc_fuel_rqrt(fuel_rqrd);
+        }
+        total+=local;
+    }
+    return total;
 }
 
 int main(void) {
-    int total = calc_total_fuel();
-    printf("total fuel required for part 1 is: %i\n", total);
+    FILE *fptr;
+
+    char filename[] = "data/day1.txt";
+    fptr = fopen(filename, "r");
+
+    int fuel_non_recurs = calc_total_fuel(fptr);
+    fseek(fptr, 0, 0);
+
+    int fuel_recurs = calc_fuel_recursive(fptr);
+    fclose(fptr);
+
+    printf("total fuel required for part 1 is: %i\n", fuel_non_recurs);
+    printf("total fuel required for part 2 is: %i\n", fuel_recurs);
     return 0;
 }
 
