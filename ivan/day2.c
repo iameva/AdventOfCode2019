7aba3ab21b7f8379eb46b99699165b39d1bd7398
ivanwakeup <ivan9miller@gmail.com>
2019-12-03 19:20:35 -0700

    ugghhhh

--- ivan/day2.c
+++ ivan/day2.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 FILE* read_input() {
     char filename[] = "data/day2.txt";
     return fopen(filename, "r");
 }
-int * to_int_array(char *arr[]) {
-    int val[5];
-    return val;
+
+int * to_int_array(char *arr) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    char buf[20];
+    int result[1000];
+    while(arr[i]!=NULL){
+        if(isdigit(arr[i])){
+            buf[j] = (int)arr[i];
+            j++;
+        }
+        else {
+          int to_int = atoi(buf);
+          result[k] = to_int;
+          while(j>=0) {
+            buf[j] = NULL;
+            j--;
+          }
+
+          j=0;
+          k++;
+        }
+        i++;
+    }
+    return &result;
 }
+
 int main(void) {
     FILE *fptr = read_input();
-    char buffer[100];
+    char buffer[1000];
 
-    while(fgets(buffer, 100, fptr) != NULL){
-        printf("%s", buffer);
+    while(fgets(buffer, 1000, fptr) != NULL){
+        int * results = to_int_array(buffer);
+        int i = 0;
+        printf("%i", *results);
+//        while(results[i]!=NULL) {
+//            printf("%i", results[i]);
+//        }
     }
     fclose(fptr);
     return 0;
