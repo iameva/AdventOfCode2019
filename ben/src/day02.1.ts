c0527d6b57add60b9d699ce0e696ae9a41b1c594
Ben Smith <ben.smith@ereplacementparts.com>
2019-12-11 10:54:16 -0700

    day 2.1

--- ben/src/day02.1.ts
+++ ben/src/day02.1.ts
@@ -1,50 +1,67 @@
+export {}
+
 const fs = require('fs').promises
 const path = require('path')
 const data_path = path.resolve(__dirname, 'data', 'day02.1.txt')
 
-const intcode_methods = {
-    1: add,
-    2: multiply, 
-    99: stop,
-}
-
-function add() {
+const add = function() {
     return [].slice.call(arguments).reduce((acc, val) => {
         return acc += val
     }, 0)
 }
 
-function multiply() {
+const multiply = function() {
     return [].slice.call(arguments).reduce((acc, val) => {
+        if(acc == 0) return acc = val
         return acc *= val
     }, 0)
 }
 
-function stop() {
+const end = function(intcode) {
+    return intcode[0]
+}
 
+let intcode_methods = {
+    1: add,
+    2: multiply, 
+    99: end,
 }
 
 function run_intcode(index, intcode) {
-    if(intcode_methods[index] == undefined) {
-        throw `Unknown intcode command ${intcode[index]}`
+    console.log(index, intcode)
+    const method = intcode_methods[intcode[index]]
+    if(method == undefined) {
+        throw `Unknown intcode method ${intcode[index]}, at index ${index}`
     }
     
-    if(intcode[index] == 99) {
-        return intcode[0]
+    if(method.name == 'end') {
+        return method(intcode)
+    }
+
+    if(index + 4 > intcode.length) {
+        throw `Unable to run another intcode chunk, stopped at index ${index}`
     }
 
     // run calc 
-    intcode_methods[index](intcode[index + 1], intcode[index + 2])
+    let result = method(intcode[intcode[index + 1]], intcode[intcode[index + 2]])
+    intcode[intcode[index + 3]] = result
+
+    return run_intcode(index + 4, intcode)
 }
 
 async function run() {
     let data = await fs.readFile(data_path, "binary")
-    // data = data
-    data = [1,0,0,0,99] 
+    data = data
         .toString()
         .split(",")
+        .map((val) => {
+            return Number(val)
+        })
 
-    
+    data[1] = 12
+    data[2] = 2
+   
+    console.log(run_intcode(0, data))
 }
 
 
