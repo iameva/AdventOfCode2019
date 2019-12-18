f3abc88ec1861a0598ebfae042125d95970a3270
Ryan Sargent <rwsargent@google.com>
2019-12-18 11:09:26 -0800

    cleaned up day 7 to use the same infra for both parts.

--- clojure/src/aoc/day07.clj
+++ clojure/src/aoc/day07.clj
@@ -7,10 +7,8 @@
 
 (defn tails [coll] (take-while seq (iterate rest coll)))
 (defn inits [coll] (reductions conj [] coll))
-
 (defn rotations [a-seq]
   (rest (map concat (tails a-seq) (inits a-seq))))
-
 (defn permutations [a-set]
   (if (empty? a-set)
     (list ())
@@ -18,68 +16,8 @@
      (fn [[x & xs]] (map #(cons x %) (permutations xs)))
      (rotations a-set))))
 
-;; (->> (range 4)
-;;      permutations
-;;      (map ) ;; turn permutations into intiailzed initial inputs
-;;      () ;; turn groupings of inputs into intcode compter instances
-;;      ()) ;; run int-code, output final value
-        ;; sort
 
-(defn input [q]
-  (.take q))
-
-(defn output [q val]
-  (do
-    (.put q val)))
-
-(defn config-amplifier [qgroup]
-  (ic/make-int-computer
-   (partial input (first qgroup))
-   (partial output (first (next qgroup)))))
-
-;; (map #(%1 memory) amplifiers)
-(defn create-queues [powers]
-  (map (fn[power]
-         (do
-           (def q (new java.util.concurrent.ArrayBlockingQueue 2))
-           (.offer q power)
-           q))
-       powers))
-
-(defn create-amplifier [io]
-  (let [in-q (first io)
-        out-q (first (next io))
-        intcode (ic/make-int-computer
-     (partial input in-q)
-     (partial output out-q))]
-    (fn [memory]
-      (do
-        (intcode memory)
-        (.peek out-q)))))
-
-;; takes in a sequence of queues 
-(defn create-amp-chains [qs]
-   (let [q-chain (concat qs [(new java.util.concurrent.ArrayBlockingQueue 2)])]
-     (do
-       (.put (first q-chain) 0)
-       (map
-        (fn [io]
-          (create-amplifier io))
-        (partition 2 1 q-chain)))))
-
-(defn amp-output [program power-seq]
-  ;; forget the aggregate, just return the last number in the sequence.
-  (reduce #(%2 program) 0 (create-amp-chains (create-queues power-seq))))
-
-(defn part-one [memory]
-  (max (->> (range 5)
-       (combo/permutations)
-       (map (partial amp-output memory)))))
- 
-
-;;; part two almost entirely different.
-
-(defn create-chans [powers]
+(defn create-channels [powers]
   (map (fn[power]
          (do
            (def c (chan (buffer 256)))
@@ -87,12 +25,8 @@
            c))
        powers))
 
-(defn input-ch [ch]
-  (<!! ch))
-
-(defn output-ch [ch valu]
-  (>!! ch val))
-
+;; Creates an instance of an IntCode computer, and returns a go channel
+;; that contains the output channel for the computer.
 (defn create-async-amplifier [io]
   (let [in-ch (first io)
         out-ch (first (next io))
@@ -104,29 +38,35 @@
         (intcode memory)
         out-ch))))
 
-(defn create-channel-ring [cs]
+;; 
+(defn create-amp-ring [cs]
   (do
-    (>!! (first cs) 0)
+    (>!! (first cs) 0) ;; add initial start to first input channel
     (map
      (fn [io]
        (create-async-amplifier io))
+     ;; Below creates a ring of channels
+     ;; Given channes C1, C2, ..., C4, C5, it will return
+     ;; pairs of adajent channels, with wrap-around.
+     ;; => [(C1 C2) (C2 C3) ... (C4 C5) (C5 C1)]
      (concat (partition 2 1 cs) [(seq [(last cs) (first cs)])]))))
 
 (defn amp-output-async [program power-seq]
   (->> power-seq
-       create-chans
-       create-channel-ring
+       create-channels
+       create-amp-ring
        (map #(%1 program))
        doall
        (map <!!)
        last
        <!!))
 
-  ;; (Map <!! (doall (map
-  ;;   #(%1 program)
-  ;;   (create-channel-ring (create-chans power-seq))))))
+(defn part-one [memory]
+  (apply max (->> (range 5)
+                  (combo/permutations)
+                  (map (partial amp-output-async memory)))))
 
 (defn part-two [memory]
-  (max (->> (range 5 10)
-            (combo/permutations)
-            (map (partial amp-output-async memory)))))
+  (apply max (->> (range 5 10)
+                  (combo/permutations)
+                  (map (partial amp-output-async memory)))))
