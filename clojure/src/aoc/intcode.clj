f1893640fc2cc021f7b439535963518cb4462d30
Ryan Sargent <rwsargent@rwsargent-macbookpro.roam.corp.google.com>
2019-12-19 23:47:11 -0800

    refactor of the intcode

--- clojure/src/aoc/intcode.clj
+++ clojure/src/aoc/intcode.clj
@@ -4,51 +4,65 @@
    [clojure.string :as str]
    [clojure.core.async :as async :refer [>!! <!! go]]))
 
+(defn evaluate-arg [state arg-with-mode]
+  (let [memory (state :ram)
+        arg (:arg arg-with-mode)
+        mode (:mode arg-with-mode)]
+    (case mode
+      0  (memory arg) ;; POSITION
+      1  arg           ;; IMMEDIATE
+      2  (memory (+ arg (state :rel-offset))))))
+
 (def instruction-template
   { ;; opcode adden adder dest
    1 {:name  "add"
       :func  (fn [state args]
                (-> state
                    (update :ram (fn [ram args]
-                                  (assoc ram (args 2) (+ (args 0) (args 1)))))
-                   args)
-               (update :pgm-ctr + 4))
+                                  (assoc ram (:arg (args 2))
+                                         (apply + (map (partial evaluate-arg state) (map args [0 1])))))
+                           args)
+                   (update :pgm-ctr + 4)))
       :size  4}
    2 {:name  "multi"
       :func  (fn [state args]
                (-> state
                    (update :ram (fn [ram args]
-                                  (assoc ram (args 2) (apply * (map args [0 1])))))
-                   (update :pgm-ctn + 4)))
+                                  (assoc ram (:arg (args 2))
+                                         (apply * (map (partial evaluate-arg state) (map args [0 1])))))
+                           args)
+                   (update :pgm-ctr + 4)))
       :size  4}
    3 {:name  "input"
       :func  (fn [state args]
                (-> state
                    (update :ram (fn [ram args]
-                                  (assoc ram (args 0) (<!! (:in state)))))
+                                  (assoc ram (:arg (args 0)) (<!! (:in state))))
+                           args)
                    (update :pgm-ctr + 2)))
       :size  2}
    4 {:name  "output"
       :func  (fn[state args]
                (do
-                 (>!! (:out state) (state :ram))
+                 (>!! (:out state) (apply (partial evaluate-arg state) args))
                  (update state :pgm-ctr + 2)))
       :size  2}
    5 {:name  "jump-if-true"
       :func  (fn [state args]
                (-> state
                    (update :pgm-ctr (fn[ctr args]
-                                      (let [a (first args)
-                                            b (last args)]
-                                        (if (not= 0 a) b (+ ctr 3))) args))))
+                                      (let [a (evaluate-arg state (first args))
+                                            b (evaluate-arg state (last args))]
+                                        (if (not= 0 a) b (+ ctr 3))) args)
+                           args)))
       :size  3}
    6 {:name  "jump-if-false"
       :func  (fn [state args]
                (update state
                        :pgm-ctr
                        (fn[ctr args]
-                         (let [a (first args)
-                               b (last args)]
+                         (let [a (evaluate-arg state (first args))
+                               b (evaluate-arg state (last args))]
                            (if (= 0 a) b (+ ctr 3))))
                        args))
       :size  3}
@@ -57,7 +71,8 @@
                (-> state
                    (update :ram
                            (fn[ram args]
-                             (assoc ram (args 2) (if (< (args 0) (args 1)) 1 0)))
+                             (assoc ram (:arg (args 2))
+                                    (if (< (evaluate-arg state (args 0)) (evaluate-arg state (args 1))) 1 0)))
                            args)
                    (update :pgm-ctr + 4)))
       :size  4}
@@ -66,15 +81,16 @@
                (-> state
                    (update :ram
                            (fn[ram args]
-                             (assoc ram (args 2) (if (= (args 0) (args 1)) 1 0)))
+                             (assoc ram (:arg (args 2))
+                                    (if (= (evaluate-arg state (args 0)) (evaluate-arg state (args 1))) 1 0)))
                            args)
                    (update :pgm-ctr + 4)))
       :size  4}
    9 {:name  "base adjust"
       :func  (fn[state args]
                (-> state
-                   (update :rel-offest
-                           (fn[offset args] (+ offset (args 0)))
+                   (update :rel-offset
+                           (fn[offset args] (+ offset (evaluate-arg state (args 0))))
                            args)
                    (update :pgm-ctr + 2)))
       :size  2}
@@ -103,33 +119,18 @@
      (-> mode-params (quot 10) (quot 10) (mod 10))
      ]))
 
-#dbg(defn decode-args [state size]
-  (subvec (:ram state) (inc (:pgm-ctr state)) (+ (:pgm-ctr state) size)))
-
-#dbg(defn evaluate-param [state arg-with-mode]
-  (let [memory (state :ram)]
-    (case (arg-with-mode 0)
-      0  (memory (arg-with-mode 1)) ;; POSITION
-      1 (arg-with-mode 1)           ;; IMMEDIATE
-      2  (memory (+ (arg-with-mode 1) (state :rel-offest)))))) ;; RELATIVE
-
-;; returns the values used in the instructions functions
-#dbg(defn evaluate-params [state, args, modes]
-  (into [] (map (partial evaluate-param state) (map vector modes args))))
-
-;; Execute instruction function against resolved parameters
-#dbg(defn execute-instruction [state instr params]
-      (let [operation (:func instr)]
-        (operation state params)))
+(defn decode-args [state instr]
+  (let [args (subvec (:ram state) (inc (:pgm-ctr state)) (+ (:pgm-ctr state) (:size instr)))
+        modes (decode-modes state)]
+    (into [] (map (fn[pair] {:mode (pair 0) :arg (pair 1)}) (map vector modes args))))) ;; RELATIVE
 
 (defn run [state instructions]
   (loop [state state]
     (if  (= "stop" (decode-instruction state instructions))
       state
       (let [instr (decode-instruction state instructions)
-            args  (decode-args state (instr :size))
-            modes (decode-modes state)]
-        (recur (execute-instruction state instr (evaluate-params state args modes)))))))
+            args  (decode-args state instr)]
+        (recur ((:func instr) state args))))))
 
 (defn make-int-computer [input-channel output-channel]
   (fn [memory]
